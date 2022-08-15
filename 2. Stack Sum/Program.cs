0dcf4d865212733bc7bb71e918e95d4e1ef83a80
Dobrin <dobrin.mail@gmail.com>
2022-08-15 22:58:02 +0300

    Add project files.

--- 2. Stack Sum/Program.cs
+++ 2. Stack Sum/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _2._Stack_Sum
 {
@@ -6,7 +8,38 @@ namespace _2._Stack_Sum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>(input);
+
+            string command = Console.ReadLine().ToLower();
+
+            while (command != "end")
+            {
+                string[] tokens = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (tokens[0] == "add")
+                {
+                    stack.Push(int.Parse(tokens[1]));
+                    stack.Push(int.Parse(tokens[2]));
+                }
+                if (tokens[0] == "remove")
+                {
+                    if (stack.Count >= int.Parse(tokens[1]))
+                    {
+                        for (int i = 0; i < int.Parse(tokens[1]); i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+                command = Console.ReadLine().ToLower();
+            }
+            int sum = 0;
+            while (stack.Count > 0)
+            {
+                sum += stack.Pop();
+            }
+            Console.WriteLine($"Sum: {sum}");
         }
     }
 }
---
+++ 4. Matching Brackets/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(' )
+                {
+                    stack.Push(i);
+                }
+                if (expression[i] == ')')
+                {
+                    int startIndex = stack.Pop();
+                    int lastIndex = i;
+
+                    Console.WriteLine(expression.Substring(startIndex, lastIndex - startIndex + 1));
+                }
+            }
+        }
+    }
+}
