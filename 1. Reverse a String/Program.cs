d68eb1585ebc796654509d0235bba20d30500206
Dobrin <dobrin.mail@gmail.com>
2022-08-15 22:07:18 +0300

    Add project files.

---
+++ 1. Reverse a String/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _1._Reverse_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            string input = Console.ReadLine();
+
+            foreach (var item in input)
+            {
+                stack.Push(item);
+            }
+
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
