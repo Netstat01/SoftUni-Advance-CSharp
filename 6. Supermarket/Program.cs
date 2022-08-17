b1c521af7cf95a16dd3398e9816250c32f5bb2a0
Dobrin <dobrin.mail@gmail.com>
2022-08-17 16:55:43 +0300

    Add project files.

---
+++ 6. Supermarket/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6._Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Queue<string> names = new Queue<string>();
+
+            while (true)
+            {
+                if (input == "End")
+                {
+                    break;
+                }
+                else if (input == "Paid")
+                {
+                    foreach (var item in names)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    names.Clear();
+                    input = Console.ReadLine();
+                }
+                else
+                {                    
+                    names.Enqueue(input);
+                    input = Console.ReadLine();
+                }                
+            }
+            Console.WriteLine($"{names.Count} people remaining.");
+        }
+    }
+}
